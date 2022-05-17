9e709c808625d343ed35616fb631261dd6d90a73
zuihoudefangzong <58388720@qq.com>
2022-05-17 10:31:46 +0800

    order-list数据渲染

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -16,7 +16,7 @@
                     <a href="javascript:;" v-if="username">{{username}}</a>
                     <a href="javascript:;" v-if="!username" @click="login">登录</a>
                     <a href="javascript:;" v-if="username" @click="logout">退出</a>
-                    <a href="javascript:;" v-if="username">我的订单</a>
+                    <a href="/#/order/list" v-if="username">我的订单</a>
                     <a href="javascript:;" v-if="!username">注册</a>
                     <!-- 购物车有个icon图标 -->
                     <a href="javascript:;" class="my-cart" @click="goToCart">
--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -1,5 +1,182 @@
 <template>
-    <div>
-        orderList
+  <div class="order-list">
+    <div class="wrapper">
+      <div class="container">
+        <div class="order-box">
+          <div class="order" v-for="(order, index) in list" :key="index" >
+            <!-- 订单信息 -->
+            <div class="order-title">
+              <div class="item-info fl">
+                {{order.createTime}}
+                <span>|</span>
+                {{order.receiverName}}
+                <span>|</span>
+                订单号：{{order.orderNo}}
+                <span>|</span>
+                {{order.paymentTypeDesc}}
+              </div>
+              
+              <div class="item-money fr">
+                <span>应付金额：</span>
+                <span class="money">{{order.payment}}</span>
+                <span>元</span>
+              </div>
+            </div>
+            <!-- 每个订单多个商品 -->
+            <div class="order-content clearfix">
+              <div class="good-box fl">
+                <div
+                  class="good-list"
+                  v-for="(item,i) in order.orderItemVoList"
+                  :key="i"
+                >
+                  <div class="good-img">
+                    <img v-lazy="item.productImage">
+                  </div>
+                  <div class="good-name">
+                    <div class="p-name">{{item.productName}}</div>
+                    <div class="p-money">{{item.totalPrice + 'X' + item.quantity}}</div>
+                  </div>
+                </div>
+              </div>
+              <div class="good-state fr" v-if="order.status == 20">
+                <a href="javascript:;">{{order.statusDesc}}</a>
+              </div>
+              <div class="good-state fr" v-else>
+                <a href="javascript:;" @click="goPay(order.orderNo)">{{order.statusDesc}}</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+<script>
+  export default{
+    name:'OrderList',
+    data() {
+      return {
+        loading: false,
+        list:[],//订单list
+        pageSize:10,
+        pageNum:1,//分页
+        total:0,//总数量
+      }
+    },
+    mounted() {
+      this.getOrderList()
+    },
+    methods: {
+      getOrderList(){
+        this.loading = true;
+        this.axios.get('/orders',{
+          params:{
+            pageSize:10,
+            pageNum:this.pageNum
+          }
+        }).then((res)=>{
+          this.loading = false;
+          this.list = this.list.concat(res.list);
+          this.total = res.total;
+          this.showNextPage = res.hasNextPage;
+        })
+    },
+    goPay(orderNo){
+        // 三种路由跳转方式
+        // this.$router.push('/order/pay')
+        /*this.$router.push({
+          name:'order-pay',
+          query:{
+            orderNo
+          }
+        })*/
+        this.$router.push({
+          path:'/order/pay',
+          query:{
+            orderNo
+          }
+        })
+      },
+  }
+}
+</script>
+<style lang="scss">
+  @import './../assets/scss/config.scss';
+  @import './../assets/scss/mixin.scss';
+  .order-list{
+    .wrapper{
+      background-color:$colorJ;
+      padding-top:33px;
+      padding-bottom:110px;
+      .order-box{
+        .order{
+          @include border();
+          background-color:$colorG;
+          margin-bottom:31px;
+          &:last-child{
+            margin-bottom:0;
+          }
+          .order-title{
+            @include height(74px);
+            background-color:$colorK;
+            padding:0 43px;
+            font-size:16px;
+            color:$colorC;
+            .item-info{
+              span{
+                margin:0 9px;
+              }
+            }
+            .money{
+              font-size:26px;
+              color:$colorB;
+            }
+          }
+          .order-content{
+            padding:0 43px;
+            .good-box{
+              width:88%;
+              .good-list{
+                display: flex;
+                align-items: center;
+                height:145px;
+                .good-img{
+                  width:112px;
+                  img{
+                    width:100%;
+                  }
+                }
+                .good-name{
+                  font-size:20px;
+                  color:$colorB;
+                }
+              }
+            }
+            .good-state{
+              @include height(145px);
+              font-size: 20px;
+              color:$colorA;
+              a{
+                color:$colorA;
+              }
+            }
+          }
+        }
+        .pagination{
+          text-align:right;
+        }
+        .el-pagination.is-background .el-pager li:not(.disabled).active{
+          background-color: #FF6600;
+        }
+        .el-button--primary{
+          background-color: #FF6600;
+          border-color: #FF6600;
+        }
+        .load-more,.scroll-more{
+          text-align:center;
+        }
+      }
+    }
+  }
+</style>
